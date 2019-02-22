58d88e0ad45ff97f1ad6f0dfbee920a767585bee
Kelsey Low <kklowlow@gmail.com>
2019-02-22 02:09:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,9 @@
 class Book 
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :page_count
   
-  def initialize(title, author)
+  def initialize(title, author, page_count)
     @title = title
     @author = author
+    @page_count = page_count
   end
 end
